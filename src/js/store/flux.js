fd0c8cfc8ce74ec98190b34967ab2e0033f71ec4
Jorge22fv <jorge.22fv@gmail.com>
2022-07-27 02:48:14 +0000

    casa Slytherin 50%

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,11 +1,68 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			
-			
+			slytherinCharacters : [],
+			hufflepuffCharacters: [],
+			ravenclawCharacters : [],
+			gryffindorCharacters: [],	
 		},
 		actions: {
-			
+			getSlytherinCharacters: () =>{
+				var requestOptions = {
+					method: 'GET',
+					redirect: 'follow'
+				  };
+				  
+				  fetch("https://hp-api.herokuapp.com/api/characters/house/slytherin", requestOptions)
+					.then(response => response.json())
+					.then(result => setStore({
+						slytherinCharacters: result
+					}))
+					.catch(error => console.log('error', error));
+			},
+
+			getHufflepufCharacters: () =>{
+				var requestOptions = {
+					method: 'GET',
+					redirect: 'follow'
+				  };
+				  
+				  fetch("https://hp-api.herokuapp.com/api/characters/house/hufflepuff", requestOptions)
+					.then(response => response.json())
+					.then(result => setStore({
+						hufflepuffCharacters: result
+					}))
+					.catch(error => console.log('error', error));
+			},
+
+			getRavenclawCharacters: () =>{
+				var requestOptions = {
+					method: 'GET',
+					redirect: 'follow'
+				  };
+				  
+				  fetch("https://hp-api.herokuapp.com/api/characters/house/ravenclaw", requestOptions)
+					.then(response => response.json())
+					.then(result => setStore({
+						ravenclawCharacters: result
+					}))
+					.catch(error => console.log('error', error));
+			},
+
+			getGryffindorCharacters: ()=>{
+				var requestOptions = {
+					method: 'GET',
+					redirect: 'follow'
+				  };
+				  
+				  fetch("https://hp-api.herokuapp.com/api/characters/house/gryffindor", requestOptions)
+					.then(response => response.json())
+					.then(result => setStore({
+						gryffindorCharacters: result
+					}))
+					.catch(error => console.log('error', error));
+			},
+
 		}
 	};
 };
