a4e44e62e168bfb303fc5c9b6b5fa3bb18708c0c
Jorge22fv <jorge.22fv@gmail.com>
2022-09-05 11:52:29 +0000

    55% one house

--- src/js/component/card.js
+++ src/js/component/card.js
@@ -8,9 +8,9 @@ export const Card = ({character}) => {
         <div className="card">
           <img src={character.image} className="card-img-top"/>
           <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" className="btn btn-primary">Go somewhere</a>
+            <h5 className="card-title">{character.name}</h5>
+            <p className="card-text">{character.house}.</p>
+            <a href="#" className="btn btn-primary">Ver Perfil</a>
           </div>
         </div>
       </div>
