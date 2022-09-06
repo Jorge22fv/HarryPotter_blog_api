bb797aaf65ce61ff4aaf7a740ccd748712c8b483
Jorge22fv <jorge.22fv@gmail.com>
2022-09-06 11:08:35 +0000

    alo

--- src/js/component/card.js
+++ src/js/component/card.js
@@ -4,9 +4,10 @@ export const Card = ({character}) => {
 
     return (
 
-      <div>
+      <div  className="card container me-2"
+      style={{ minWidth: "400px" }}>
         <div className="card">
-          <img src={character.image} className="card-img-top"/>
+          <img src={character.image} className="card-img-top m-none p-none"/>
           <div className="card-body">
             <h5 className="card-title">{character.name}</h5>
             <p className="card-text">{character.house}.</p>
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -3,7 +3,11 @@ import "../../styles/home.css";
 import {CharacterList} from "../component/characterList"
 
 export const Home = () => (
-	<div className="text-center mt-5">
+	<div className="text-center mt-2 home" >
+		<h2 className="text-center m-4">House Slytherin</h2>
+		<div style={{ display: "flex", minWidth: "auto", textAlign: "start" }}
+        className="overflow-scroll">
 		<CharacterList/>
+		</div>
 	</div>
 );
